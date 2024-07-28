718f003ee9fcd01c9c1a399af2c1bf1ecc7f03cc
d-kasahara <93462352+d-kasahara@users.noreply.github.com>
2024-07-28 17:53:51 +0900

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -21,13 +21,14 @@ export const localSendDmTestApi = localSendDmTest;
 //     }
 //   });
 
+// ターゲットユーザーのIDを抽出する用関数
 export const searchTargetUser = functions
   .region('asia-northeast1')
   .pubsub.schedule('0 6 * * *')
   // .pubsub.schedule('30 * * * *')
   .timeZone('Asia/Tokyo')
   .onRun(async () => {
-    await searchByHashtag(0, 'フリーモデル', 100);
+    await searchByHashtag(0, 'フリーモデル', 100); // 条件表のアカウントリストのステータス「運用中」の上から1番目
     functions.logger.info('Function稼働中');
     return;
   });
@@ -42,42 +43,46 @@ export const searchTargetUser = functions
 //     return;
 //   });
 
+// 送信関数1
 export const sendDm1 = functions
   .region('asia-northeast1')
-  .pubsub.schedule('5 * * * *')
+  .pubsub.schedule('5 * * * *') // 毎時5分に実行
   .timeZone('Asia/Tokyo')
   .onRun(async () => {
-    await sendDmByAccount(1);
+    await sendDmByAccount(1);    // 条件表のアカウントリストのステータス「運用中」の上から2番目
     functions.logger.info('Function稼働中');
     return;
   });
 
-export const sendDm2 = functions
-  .region('asia-northeast1')
-  .pubsub.schedule('10 * * * *')
-  .timeZone('Asia/Tokyo')
-  .onRun(async () => {
-    await sendDmByAccount(2);
-    functions.logger.info('Function稼働中');
-    return;
-  });
-
-export const sendDm3 = functions
-  .region('asia-northeast1')
-  .pubsub.schedule('15 * * * *')
-  .timeZone('Asia/Tokyo')
-  .onRun(async () => {
-    await sendDmByAccount(3);
-    functions.logger.info('Function稼働中');
-    return;
-  });
-
-// export const sendDm4 = functions
+// 送信関数2
+// export const sendDm2 = functions
 //   .region('asia-northeast1')
-//   .pubsub.schedule('20 * * * *')
+//   .pubsub.schedule('10 * * * *') // 毎時10分に実行
 //   .timeZone('Asia/Tokyo')
 //   .onRun(async () => {
-//     await sendDmByAccount(4);
+//     await sendDmByAccount(2);  // 条件表のアカウントリストのステータス「運用中」の上から3番目
+//     functions.logger.info('Function稼働中');
+//     return;
+//   });
+
+// 送信関数3
+// export const sendDm3 = functions
+//   .region('asia-northeast1')
+//   .pubsub.schedule('15 * * * *')
+//   .timeZone('Asia/Tokyo')
+//   .onRun(async () => {
+//     await sendDmByAccount(3);  // 条件表のアカウントリストのステータス「運用中」の上から4番目
+//     functions.logger.info('Function稼働中');
+//     return;
+//   });
+
+// 送信関数4
+// export const sendDm4 = functions
+//   .region('asia-northeast1')
+//   .pubsub.schedule('20 * * * *') // 毎時20分に実行
+//   .timeZone('Asia/Tokyo')
+//   .onRun(async () => {
+//     await sendDmByAccount(4);  // 条件表のアカウントリストのステータス「運用中」の上から5番目
 //     functions.logger.info('Function稼働中');
 //     return;
 //   });
